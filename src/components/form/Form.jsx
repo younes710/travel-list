98e75a382bf4ef5da00b123cc7caa121c75f834c
Younes Abdi <realyoonesabdi@gmail.com>
2023-09-13 11:25:30 +0330

    Controlled form elements

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,14 +1,29 @@
+import { useState } from 'react';
 import styles from './form.module.css';
 
 const Form = () => {
+    const [desc, setDesc] = useState('');
+    const [quantity, setQuantity] = useState(1);
+
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        if (!desc) return;
+
+        const newItem = { desc, quantity, packed: false, id: Date.now() };
+        console.log(newItem);
+
+        setDesc('');
+        setQuantity(1);
     };
 
     return (
         <form className={styles['add-form']} onSubmit={handleSubmit}>
             <h3>What do you need for your 😍 trip?</h3>
-            <select>
+            <select
+                value={quantity}
+                onChange={(e) => setQuantity(Number(e.target.value))}
+            >
                 {Array.from({ length: 20 }, (_, i) => i + 1).map(
                     (num, index) => (
                         <option value={num} key={num}>
@@ -17,7 +32,12 @@ const Form = () => {
                     )
                 )}
             </select>
-            <input type='text' placeholder='Item...' />
+            <input
+                type='text'
+                placeholder='Item...'
+                value={desc}
+                onChange={(e) => setDesc(e.target.value)}
+            />
             <button>Add</button>
         </form>
     );
