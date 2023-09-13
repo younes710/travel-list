0e62c6d93f859bae65fa671a2ecfd293175eb068
Younes Abdi <realyoonesabdi@gmail.com>
2023-09-13 09:26:27 +0330

    Add PackingList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Form from './components/form/Form';
 import Logo from './components/logo/Logo';
+import PackingList from './components/packing-list/PackingList';
 
 function App() {
     return (
         <>
             <Logo />
             <Form />
+            <PackingList />
         </>
     );
 }
---
+++ src/components/packing-list/PackingList.jsx
@@ -0,0 +1,7 @@
+import styles from './packingList.module.css';
+
+const PackingList = () => {
+    return <div className={styles['list']}>List</div>;
+};
+
+export default PackingList;
