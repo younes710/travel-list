2c7faa083b470e03254651d8ce6511bc9df513d6
Younes Abdi <realyoonesabdi@gmail.com>
2023-09-15 20:15:43 +0330

    Feat sorting items

--- src/components/packing-list/PackingList.jsx
+++ src/components/packing-list/PackingList.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import Item from '../item/Item';
 import styles from './packingList.module.css';
 
 const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
+    const [sortBy, setSortBy] = useState('input');
+
+    let sortedItems;
+
+    if (sortBy === 'input') sortedItems = items;
+
+    if (sortBy === 'description')
+        sortedItems = items
+            .slice()
+            .sort((a, b) => a.description.localeCompare(b.description));
+
+    if (sortBy === 'packed')
+        sortedItems = items
+            .slice()
+            .sort((a, b) => Number(a.packed) - Number(b.packed));
+
     return (
         <div className={styles['list']}>
             <ul>
-                {items.map((item, index) => (
+                {sortedItems.map((item, index) => (
                     <Item
                         item={item}
                         key={item.id}
@@ -14,6 +31,16 @@ const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
                     />
                 ))}
             </ul>
+            <div className='actions'>
+                <select
+                    value={sortBy}
+                    onChange={(e) => setSortBy(e.target.value)}
+                >
+                    <option value='input'>Sort by input order</option>
+                    <option value='description'>Sort by description</option>
+                    <option value='packed'>Sort by packed status</option>
+                </select>
+            </div>
         </div>
     );
 };
