04f7edb605cb77a532771edd525ab6fbf827a3e2
Younes Abdi <realyoonesabdi@gmail.com>
2023-09-15 18:14:59 +0330

    Calculating statistics as derived state

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -1,10 +1,24 @@
 import styles from './stats.module.css';
 
-const Stats = () => {
+const Stats = ({ items }) => {
+    if (!items.length)
+        return (
+            <p className={styles['stats']}>
+                <em>Start adding some items to your packing list 🚀</em>
+            </p>
+        );
+
+    const numItems = items.length;
+    const numPacked = items.filter((item) => item.packed).length;
+    const percentage = Math.round((numPacked / numItems) * 100);
+
     return (
         <footer className={styles['stats']}>
             <em>
-                💼 You have X items on your list, and you already packed X (X%)
+                {percentage === 100
+                    ? 'You got everything! Ready to go ✈️'
+                    : `💼 You have ${numItems} items on your list, and you already
+                packed ${numPacked} (${percentage}%)`}
             </em>
         </footer>
     );
